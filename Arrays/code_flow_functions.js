9445d5ad00e83699a8367684e50f5871223c8ada
katoltmans <katoltmans@msn.com>
2022-06-06 14:10:58 -0400

    Add Code Flow examples

---
+++ Arrays/code_flow_functions.js
@@ -0,0 +1,51 @@
+var x = 5;
+
+function setX(newValue) {
+    x = newValue;
+}
+
+console.log(x);
+setX(15);
+console.log(x);
+console.log();
+
+// result will be:
+//     5
+//     15
+
+var x = 5;
+
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there"); //NOTE: this is not returned and only exists inside this function
+}
+
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
+console.log();
+
+// result will be:
+//      5
+//      -5
+//      5
+
+// Function to check for a panindrome
+function isPal(arr) {
+    for (var left = 0; left < arr.length / 2; left++) {
+        var right = arr.length - 1 - left;
+        if (arr[left] != arr[right]) {
+            return "Not a pal-indrome!";
+        }
+    }
+    return "Pal-indrome!";
+}
+
+var result1 = isPal([1, 1, 2, 2, 1]);
+console.log(result1);
+// result will be: Not a pal-indrome!
+
+var result2 = isPal([3, 2, 1, 1, 2, 3]);
+console.log(result2);
+// result will be Pal-indrome!

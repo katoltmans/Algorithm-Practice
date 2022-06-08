5cb354ba290057b377ec6587749406bf6bb2d9b9
katoltmans <katoltmans@msn.com>
2022-06-08 14:14:43 -0400

    Add getDigits()

--- Strings And Associative Arrays/strings_to_do_1.js
+++ Strings And Associative Arrays/strings_to_do_1.js
@@ -16,3 +16,20 @@ console.log(removeBlanks("I can not BELIEVE it's not BUTTER")); //output: Icanno
 console.log();
 
 // Get Digits
+const getDigits = (str) => {
+    letArr = str.split("");
+    newArr = [];
+    for (var i = 0; i < letArr.length; i++) {
+        if (!isNaN(letArr[i])) {
+            newArr.push(letArr[i]);
+        }
+    }
+    newStr = newArr.join("");
+    return newStr;
+};
+
+console.log(getDigits("abc8c0d1ngd0j0!8")); //output: 801008
+console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0")); //output: 01357924680
+console.log();
+
+// Acronyms

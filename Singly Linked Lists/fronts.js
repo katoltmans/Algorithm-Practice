0667ceddcc0e416dfc3dd4ab73bddb0d37079380
katoltmans <katoltmans@msn.com>
2022-06-15 17:47:54 -0400

    Add front()

--- Singly Linked Lists/fronts.js
+++ Singly Linked Lists/fronts.js
@@ -54,6 +54,15 @@ class SLL {
     }
 
     // Method to return the value of the head of the list
+    front() {
+        if (this.head == null) {
+            console.log(this.head);
+            return null;
+        }
+        let dataValue = this.head.data;
+        console.log(dataValue);
+        return dataValue;
+    }
 }
 
 let SLLTest = new SLL();
@@ -84,3 +93,8 @@ console.log("Remove Front");
 SLL1.removeFront(); //Returns: Node { data: 5, next: Node { data: 18, next: null } }
 SLL1.removeFront(); //Returns: Node { data: 18, next: null }
 console.log();
+
+console.log("Front");
+SLL1.front(); //Returns: 18
+SLL1.removeFront(); //Returns: null
+SLL1.front(); //Returns: null

08a9fe1f5e21446615dde72529cb714579f50ea6
katoltmans <katoltmans@msn.com>
2022-06-16 21:43:35 -0400

    Add minMaxAverage.js

--- Singly Linked Lists/display.js
+++ Singly Linked Lists/display.js
@@ -1,1 +1,41 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
+    // Method to add a node to the front of a singly linked list
+    addFront(value) {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        console.log(this.head);
+
+        return this.head;
+    }
+
+    // Method to display the nodes in a singly linked list
+    display() {
+        let dataDisplay = "";
+        let runner = this.head;
+        while (runner != null) {
+            dataDisplay += ", " + str(this.data);
+            runner = runner.next;
+        }
+        console.log(dataDisplay);
+        return dataDisplay;
+    }
+}
+
+SLLNew = new LinkedList();
+
+SLLNew.addFront(76);
+SLLNew.addFront(2);
+SLLNew.display();
+SLLNew.addFront(11.41);
+SLLNew.display();
---
+++ Singly Linked Lists/minMaxAverage.js
@@ -0,0 +1,84 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to add a node to the front of a singly linked list
+    addFront(value) {
+        let newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        console.log(this.head);
+
+        return this.head;
+    }
+
+    // Method to locate the max value in a singly linked list
+    max() {
+        let temp = 0;
+        let runner = this.head;
+        while (runner !== null) {
+            if (runner.data > temp) {
+                temp = runner;
+                runner = runner.next;
+            }
+        }
+        console.log(temp);
+        return temp;
+    }
+
+    // Method to locate the min value in a singly linked list
+    min() {
+        let temp = 900000000000000;
+        let runner = this.head;
+        while (runner !== null) {
+            if (runner.data < temp) {
+                temp = runner;
+                runner = runner.next;
+            }
+        }
+        console.log(temp);
+        return temp;
+    }
+
+    // Method to calculate the average of all values in a singly linked list
+    average() {
+        let runner = this.head;
+        let sum = 0;
+        let count = 0;
+        while (runner !== null) {
+            sum += runner.data;
+            count++;
+            runner = runner.next;
+        }
+        let average = sum / count;
+        console.log(average);
+        return average;
+    }
+}
+
+SLLNew = new LinkedList();
+
+SLLNew.addFront(48.5);
+SLLNew.addFront(21);
+SLLNew.addFront(1000);
+SLLNew.addFront(-7);
+SLLNew.addFront(-14);
+
+console.log("Max value");
+SLLNew.max();
+console.log();
+
+console.log("Min value");
+SLLNew.min();
+console.log();
+
+console.log("Average");
+SLLNew.average();
+console.log();

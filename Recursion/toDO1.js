d2e3bd994fa54cb39a6f79f7508865ab6ff8e23f
katoltmans <katoltmans@msn.com>
2022-07-02 18:07:37 -0400

    Add rSigma algorithm

--- Recursion/toDO1.js
+++ Recursion/toDO1.js
@@ -1,4 +1,16 @@
 // Recursive Sigma - when given a number, function sums all integers from 1 to that number
-const rSigma = (num) => {};
+const rSigma = (num) => {
+    if (num <= 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+    let sum = num + rSigma(num - 1);
+    return sum;
+};
 
+console.log("Recursive Sigma");
 console.log(rSigma(4));
+console.log(rSigma(10));
+console.log();

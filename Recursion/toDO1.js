a871fbf5d45b587c94be2b18b49574a7d9f28686
katoltmans <katoltmans@msn.com>
2022-07-02 18:12:26 -0400

    Add rFact algorithm

--- Recursion/toDO1.js
+++ Recursion/toDO1.js
@@ -14,3 +14,20 @@ console.log("Recursive Sigma");
 console.log(rSigma(4));
 console.log(rSigma(10));
 console.log();
+
+//Recursive Factorial -  when given a number, function finds the product of all integers from 1 to that number
+const rFact = (num) => {
+    if (num <= 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+    let sum = Math.floor(num) * rFact(Math.floor(num - 1));
+    return sum;
+};
+
+console.log("Recursive Sigma");
+console.log(rFact(3));
+console.log(rFact(6.5));
+console.log();

d97b472eb630bfedb015ba2b82004b83e0f13318
katoltmans <katoltmans@msn.com>
2022-07-08 15:27:10 -0400

    Add second-largest algorithm

--- Arrays/arrays_to_do_3.js
+++ Arrays/arrays_to_do_3.js
@@ -18,7 +18,62 @@ const removeNegatives = (arr) => {
     return arr;
 };
 
+console.log("Remove Negatives");
 console.log(removeNegatives([1, -4, 6, 8, -7, -3, 10]));
 console.log(removeNegatives([-100]));
 console.log(removeNegatives([-17, 12, 15, -18, -10000008, 100000578, -35]));
 console.log(removeNegatives([-17, 12, 15, -18, 33, 42, 51]));
+console.log();
+
+// Second-to-last
+
+const secondToLast = (arr) => {
+    // If there are less than 2 items in an array, return null
+    if (arr[arr.length - 2] === undefined) {
+        return null;
+    } else {
+        // If more than two items exits in an array, return the second to last item
+        console.log(arr);
+        return arr[arr.length - 2];
+    }
+};
+
+console.log("Second To Last");
+console.log(secondToLast([42, true, 4, "Kate", 7]));
+console.log(secondToLast["one"]);
+console.log(secondToLast[("one", 2)]);
+console.log();
+
+//second -Largest
+
+const secondLargest = (arr) => {
+    let temp;
+    let largest = arr[1];
+    let second = arr[0];
+
+    // If there are less than 2 items in an array, return null
+    if (arr[arr.length - 2] === undefined) {
+        return null;
+    } else if (largest < second) {
+        // Swap largest and second if arr[0] is greater than arr[1]
+        temp = largest;
+        largest = second;
+        second = temp;
+    }
+
+    for (let i = 2; i < arr.length; i++) {
+        if (arr[i] > second && arr[i] > largest) {
+            temp = largest;
+            largest = arr[1];
+            second = temp;
+        } else if (arr[i] > second && arr[i] < largest) {
+            second = arr[i];
+        }
+    }
+    return second;
+};
+
+console.log("Second Largest");
+console.log(secondLargest([42, 1, 4, Math.PI, 7]));
+console.log(secondLargest([42]));
+console.log();

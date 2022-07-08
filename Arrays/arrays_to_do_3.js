b19cfd5107cdb978737e98e2feb61d15942e1de7
katoltmans <katoltmans@msn.com>
2022-07-08 15:44:20 -0400

    Add Nth to last algorithm

--- Arrays/arrays_to_do_3.js
+++ Arrays/arrays_to_do_3.js
@@ -29,19 +29,18 @@ console.log();
 
 const secondToLast = (arr) => {
     // If there are less than 2 items in an array, return null
-    if (arr[arr.length - 2] === undefined) {
+    if (arr[1] === undefined) {
         return null;
-    } else {
-        // If more than two items exits in an array, return the second to last item
-        console.log(arr);
-        return arr[arr.length - 2];
     }
+
+    // If more than two items exits in an array, return the second to last item
+    return arr[arr.length - 2];
 };
 
 console.log("Second To Last");
 console.log(secondToLast([42, true, 4, "Kate", 7]));
-console.log(secondToLast["one"]);
-console.log(secondToLast[("one", 2)]);
+console.log(secondToLast(["one"]));
+console.log(secondToLast(["one", 2]));
 console.log();
 
 //second -Largest
@@ -52,9 +51,11 @@ const secondLargest = (arr) => {
     let second = arr[0];
 
     // If there are less than 2 items in an array, return null
-    if (arr[arr.length - 2] === undefined) {
+    if (arr[1] === undefined) {
         return null;
-    } else if (largest < second) {
+    }
+
+    if (largest < second) {
         // Swap largest and second if arr[0] is greater than arr[1]
         temp = largest;
         largest = second;
@@ -62,11 +63,13 @@ const secondLargest = (arr) => {
     }
 
     for (let i = 2; i < arr.length; i++) {
+        // If arr[i] is greater than both largest and second, make largest arr[i] and move largest to second
         if (arr[i] > second && arr[i] > largest) {
             temp = largest;
             largest = arr[1];
             second = temp;
         } else if (arr[i] > second && arr[i] < largest) {
+            // If arr[i] is in between largest and second, replace second with arr[i]
             second = arr[i];
         }
     }
@@ -76,4 +79,19 @@ const secondLargest = (arr) => {
 console.log("Second Largest");
 console.log(secondLargest([42, 1, 4, Math.PI, 7]));
 console.log(secondLargest([42]));
+console.log();
+
+// Nth-to-Last
+
+const nthToLast = (arr, num) => {
+    if (arr[num - 1] === undefined) {
+        return null;
+    }
+
+    return arr[arr.length - num];
+};
+
+console.log("Nth to Last");
+console.log(nthToLast([5, 2, 3, 6, 4, 9, 7], 3));
+console.log(nthToLast([1, 2, 3], 4));
 console.log();

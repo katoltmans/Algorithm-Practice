3591f8720bc68431dcff1b706dc682ce0d37c2b9
katoltmans <katoltmans@msn.com>
2022-11-14 12:01:30 -0500

    Add "Candies" algo

--- CodeSignal/introGates.js
+++ CodeSignal/introGates.js
@@ -42,14 +42,24 @@ console.log();
 
 {
     /*
+    Candies
 
-
+    n children have got m pieces of candy. They want to eat as much candy as they can, but each child must eat exactly the same amount of candy as any other child. 
+    Determine how many pieces of candy will be eaten by all the children together. Individual pieces of candy cannot be split.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function numCandy(n, m) {
+    let candyPieces = Math.floor(m / n);
+
+    let result = n * candyPieces;
+
+    return result;
+}
+
+console.log("Candies");
+console.log(numCandy(3, 10)); //expect 9
+console.log(numCandy(8, 2)); //expect 0
 console.log();
 
 {

268309c36365338cf072f8847c6cd3e1a8544aa4
katoltmans <katoltmans@msn.com>
2022-11-15 21:56:00 -0500

    Initial commit with "Reach Next Level" algo

---
+++ CodeSignal/atTheCrossroads.js
@@ -0,0 +1,113 @@
+{
+    /*
+    Reach Next Level
+
+    You are playing an RPG game. Currently your experience points (XP) total is equal to experience. 
+    To reach the next level your XP should be at least at threshold. If you kill the monster in front of you, 
+    you will gain more experience points in the amount of the reward.
+    Given values experience, threshold and reward, check if you reach the next level after killing the monster.
+    */
+}
+
+function reachLevel(experience, threshold, reward) {
+    let nextLevel = false;
+
+    if (experience + reward >= threshold) {
+        nextLevel = true;
+    }
+
+    return nextLevel;
+}
+
+console.log("Reach Next Level");
+console.log(reachLevel(10, 15, 5)); //expect true
+console.log(reachLevel(235, 293, 4)); //expect false
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();

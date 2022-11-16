2a8a560fffbdca6f7b86719def328c3e67792737
katoltmans <katoltmans@msn.com>
2022-11-16 14:56:40 -0500

    Add "Is Infinite Process?" algo
    Test whether a specified while loop will be infinite with given parameters.

--- CodeSignal/atTheCrossroads.js
+++ CodeSignal/atTheCrossroads.js
@@ -84,13 +84,33 @@ console.log();
 
 {
     /*
-    
+    Is Infinite Process?
+
+    Given integers a and b, determine whether the following pseudocode results in an infinite loop
+        while a is not equal to b do
+        increase a by 1
+        decrease b by 1
+    Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function infiniteCheck(a, b) {
+    let isInfinite = false;
+
+    while (a !== b) {
+        if (a > b) {
+            isInfinite = true;
+            break;
+        }
+        a++;
+        b--;
+    }
+    return isInfinite;
+}
+
+console.log("Is Infinite Process?");
+console.log(infiniteCheck(2, 6)); //expect false
+console.log(infiniteCheck(5, 10)); //expect true
 console.log();
 
 {

076dcb3e1c723873baeded61a069ff6cd6785cb5
katoltmans <katoltmans@msn.com>
2022-11-17 16:07:06 -0500

    Add "Tennis Set" algo
    Given two overall games won scores, determine if the tennis match has finished.

--- CodeSignal/atTheCrossroads.js
+++ CodeSignal/atTheCrossroads.js
@@ -137,13 +137,31 @@ console.log();
 
 {
     /*
-    
+    Tennis Set
+
+    In tennis, the winner of a set is based on how many games each player wins. The first player to win 6 games is declared the winner 
+    unless their opponent had already won 5 games, in which case the set continues until one of the players has won 7 games.
+    Given two integers score1 and score2, your task is to determine if it is possible for a tennis set to be finished with a final score of score1 : score2.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function possibleScore(score1, score2) {
+    let isPossible = false;
+    if (
+        (score1 === 6 && score2 < 5) ||
+        (score2 === 6 && score1 < 5) ||
+        (score1 === 7 && (score2 === 6 || score2 === 5)) ||
+        (score2 === 7 && (score1 === 6 || score1 === 5))
+    ) {
+        isPossible = true;
+    }
+
+    return isPossible;
+}
+
+console.log("Tennis Set");
+console.log(possibleScore(3, 6)); //expect true
+console.log(possibleScore(7, 2)); //expect false
 console.log();
 
 {

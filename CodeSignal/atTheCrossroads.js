9e1374d806ff1c905d157722cbe26a1eac674d3d
katoltmans <katoltmans@msn.com>
2022-11-18 17:03:12 -0500

    Add "WIll You?" algo
    Test if character belief it true given parameters

--- CodeSignal/atTheCrossroads.js
+++ CodeSignal/atTheCrossroads.js
@@ -166,13 +166,34 @@ console.log();
 
 {
     /*
-    
+    Will You?
+
+    Once Mary heard a famous song, and a line from it stuck in her head. That line was "Will you still love me when I'm no longer young and beautiful?". 
+    Mary believes that a person is loved if and only if he/she is both young and beautiful, but this is quite a depressing thought, so she wants to put 
+    her belief to the test.
+    Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+    A person contradicts Mary's belief if one of the following statements is true:
+        they are young and beautiful but not loved;
+        they are loved but not young or not beautiful.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function contradictsBelief(young, beautiful, loved) {
+    let result = false;
+
+    // console.log(typeof young, typeof beautiful, typeof loved);
+
+    if (loved === true && (young === false || beautiful === false)) {
+        result = true;
+    } else if (loved === false && young === true && beautiful === true) {
+        result = true;
+    }
+    return result;
+}
+
+console.log("Will You?");
+console.log(contradictsBelief(true, true, true)); //expect false
+console.log(contradictsBelief(false, true, true)); //expect true
 console.log();
 
 {

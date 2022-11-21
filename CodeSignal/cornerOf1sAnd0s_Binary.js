ebdc3f8725e033455fe0ca875bb8bf92b203ba42
katoltmans <katoltmans@msn.com>
2022-11-21 15:15:33 -0500

    Add "Array Packing" algo
    Pack integers into one number with least significant bits at the start of the number.

--- CodeSignal/cornerOf1sAnd0s_Binary.js
+++ CodeSignal/cornerOf1sAnd0s_Binary.js
@@ -23,13 +23,34 @@ console.log();
 
 {
     /*
-    
+    Array Packing
+
+    You are given an array of up to four non-negative integers, each less than 256.
+    Your task is to pack these integers into one number M in the following way:
+        The first element of the array occupies the first 8 bits of M;
+        The second element occupies next 8 bits, and so on.
+    Return the obtained integer M.
+
+    Note: the phrase "first bits of M" refers to the least significant bits of M - the right-most bits of an integer.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function packArray(a) {
+    // Convert to bits and combine into string in reverse order
+    // reconvert back to new number
+
+    let newNum = "";
+
+    for (let i = a.length - 1; i >= 0; i--) {
+        newNum += a[i].toString(2).padStart(8, 0);
+    }
+
+    return parseInt(newNum, 2);
+}
+
+console.log("Array Packing");
+console.log(packArray([24, 85, 0])); //expect 21784
+console.log(packArray([5])); //expect 5
 console.log();
 
 {

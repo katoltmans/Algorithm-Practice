82b6a2d285fd2c1716e9a1ee9b502f919d8ee1c0
katoltmans <katoltmans@msn.com>
2022-11-21 14:51:53 -0500

    Add "Kill K-th Bit" algo
    Convert the bit of a given parameter and return the new number

--- CodeSignal/cornerOf1sAnd0s_Binary.js
+++ CodeSignal/cornerOf1sAnd0s_Binary.js
@@ -1,12 +1,24 @@
 {
     /*
-    
+    Kill K-th Bit
+
+    In order to stop the Mad Coder evil genius you need to decipher the encrypted message he sent to his minions. 
+    The message contains several numbers that, when typed into a supercomputer, will launch a missile into the sky blocking out the sun, 
+    and making all the people on Earth grumpy and sad.
+
+    You figured out that some numbers have a modified single digit in their binary representation. 
+    More specifically, in the given number n the kth bit from the right was initially set to 0, but its current value might be different. 
+    It's now up to you to write a function that will change the kth bit of n back to 0.
     */
 }
 
-console.log("");
-console.log(); //expect
-console.log(); //expect
+function convertKthBit(n, k) {
+    return n & ~(1 << (k - 1));
+}
+
+console.log("Kill K-th Bit");
+console.log(convertKthBit(37, 3)); //expect 33
+console.log(convertKthBit(2147483647, 16)); //expect 2147450879
 console.log();
 
 {

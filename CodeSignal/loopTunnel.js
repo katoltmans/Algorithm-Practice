64379a5ef576d7fa54a8791ce77e6383aa55f68e
katoltmans <katoltmans@msn.com>
2022-12-02 21:35:34 -0500

    Initial commit

---
+++ CodeSignal/loopTunnel.js
@@ -0,0 +1,98 @@
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
+
+{
+    /*
+    
+    */
+}
+
+console.log("");
+console.log(); //expect
+console.log(); //expect
+console.log();
